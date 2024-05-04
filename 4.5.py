e53342bb1c6548420f5eaa6270d0818ec594ddae
RAHUL KUMAR <147340802+Singhrahul2511@users.noreply.github.com>
2024-05-04 10:38:43 +0530

    commit

---
+++ 4.5.py
@@ -0,0 +1,23 @@
+# # s = "The Terrible Tiger Tore The Towel!"
+# # pos = s.find('T',0)
+# # print(pos,s[pos])
+# # pos =s.find('T',pos+1)
+# # print(pos,s[pos])
+
+# # #trimming
+# # s2= '    Flanked by spaces on either side     '
+# # print(s2.lstrip())
+
+# #write a program to generate following o/p from string 'Shenanigan'
+
+# s ='shenanigan'
+# print(s[0],s[1])
+# print(s.find('shenanigan'))
+# print(s[0],s[4],s[8])
+# print(type('shenanigan'))
+
+s = "Light travel faster than sound"
+a =len(s)
+print(a)
+d ='travel'
+print(d.upper())
